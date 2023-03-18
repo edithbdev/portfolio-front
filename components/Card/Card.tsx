234cb1ed4071e062ea839ed17579b592678ee936
edithbdev <edith.toubi@gmail.com>
2023-03-18 18:51:56 +0100

    feat(card):ajout du spinner sur la card lors du clic

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,4 +1,6 @@
 import Thumbnail from '../Thumbnail/Thumbnail'
+import Spinner from '../Spinner/Spinner';
+import { useState, useEffect } from 'react'
 
 type Item = {
   name: string;
@@ -11,31 +13,60 @@ type Props = {
   displayOneList?: Item[];
 };
 
-const Card = ({ imgUrl, title, subtitle, displayOneList }: Props) => (
-  <div className='h-80'>
-    <div className='relative h-full transition-all duration-300 rounded-lg cursor-pointer'>
-      <Thumbnail imgUrl={imgUrl} height='h-72' border='border-2 border-zinc-800' />
-      <div className='absolute w-full bottom-0 px-4 py-2 rounded-b-xl bg-zinc-800'>
-        <h1 className='text-amber-200 font-semibold text-center text-sm capitalize md:uppercase'>{title}</h1>
-        {subtitle ? <p className='text-amber-400 text-center text-xs truncate' dangerouslySetInnerHTML={{ __html: subtitle }} /> : null}
-        {displayOneList && (
-          <ul className='flex justify-center flex-wrap'>
-            {displayOneList.map((item, index) => (
-              <li key={index} className='text-amber-400 text-center text-xs p-1'>
-                {index < 11 ? item.name : index === 12 && '...'}
-              </li>
-            ))}
-          </ul>
-        )}
-        <div className='flex justify-center'>
-          <button className='flex justify-center w-4/5 md:w-3/5 bg-amber-700 text-sm text-gray-100 font-semibold rounded-lg px-2 py-2 mt-2 hover:bg-amber-800 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-opacity-15'>
-            En savoir plus
-          </button>
+const Card = ({ imgUrl, title, subtitle, displayOneList }: Props) => {
+  const [loading, setLoading] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(true);
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    if (loading && clicked) {
+        setLoading(false);
+      };
+  }, [loading, clicked]);
+
+
+  return (
+    <div className='h-80' onClick={handleClick}>
+      <div className='relative h-full transition-all duration-300 rounded-lg cursor-pointer' >
+        <Thumbnail imgUrl={imgUrl} height='h-72' border='border-2 border-zinc-800' />
+        <div className='absolute w-full bottom-0 px-4 py-2 rounded-b-xl bg-zinc-800'>
+          <h1 className='text-amber-200 font-semibold text-center text-sm capitalize md:uppercase'>{title}</h1>
+          {subtitle ? (
+            <p
+              className='text-amber-400 text-center text-xs truncate'
+              dangerouslySetInnerHTML={{ __html: subtitle }}
+            />
+          ) : null}
+          {displayOneList && (
+            <ul className='flex justify-center flex-wrap'>
+              {displayOneList.map((item, index) => (
+                <li key={index} className='text-amber-400 text-center text-xs p-1'>
+                  {index < 11 ? item.name : index === 12 && '...'}
+                </li>
+              ))}
+            </ul>
+          )}
+          {clicked ?  (
+             <Spinner />
+            ) : (
+              <div className='flex justify-center'>
+                <button className='flex justify-center w-4/5 md:w-3/5 bg-amber-700 text-sm text-gray-100 font-semibold rounded-lg px-2 py-2 mt-2 hover:bg-amber-800 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-opacity-15'>
+                  En savoir plus
+                </button>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
-  </div>
+  );
+};
 
-);
+
+
 
 export default Card;
